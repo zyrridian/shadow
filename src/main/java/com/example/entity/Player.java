adc73336e364833db5951ad8f4aa629a6b17e897
zyrridian <rezkyaf246@gmail.com>
2024-10-11 16:08:31 +0700

    change rectangle to sprite character

--- src/main/java/com/example/entity/Player.java
+++ src/main/java/com/example/entity/Player.java
@@ -2,8 +2,10 @@ package com.example.entity;
 
 import com.example.common.GamePanel;
 import com.example.common.KeyHandler;
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Player extends Entity {
     
@@ -15,29 +17,68 @@ public class Player extends Entity {
         this.keyHandler = keyHandler;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            up0 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_up_0.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_up_2.png"));
+            down0 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_down_0.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_down_2.png"));
+            left0 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_left_0.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_left_2.png"));
+            right0 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_right_0.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/mc_walk_right_2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if (keyHandler.upPressed == true) {
+            direction = "up";
             y -= speed;
         } else if (keyHandler.downPressed == true) {
+            direction = "down";
             y += speed;
         } else if (keyHandler.leftPressed == true) {
+            direction = "left";
             x -= speed;
         } else if (keyHandler.rightPressed == true) {
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                image = up0;
+                break;
+            case "down":
+                image = down0;
+                break;
+            case "left":
+                image = left0;
+                break;
+            case "right":
+                image = right0;
+                break;
+        }
+        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 
 }
